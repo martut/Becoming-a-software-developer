54347bee6c9b364d09e5776a242d084f009b30af
martut <martut2762@gmail.com>
2019-07-08 21:41:10 +0200

    init episode 1

---
+++ Episode1/Episode1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Episode1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
