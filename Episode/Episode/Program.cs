f4b2ef81368771f63d0d6522f5669bfc2dab96f2
martut <martut2762@gmail.com>
2019-07-21 17:09:28 +0200

    Edit Episode1 to Episode. Delegate, Lambda, Event

---
+++ Episode/Episode/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Episode1.Models;
+
+namespace Episode1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//            Console.WriteLine("Hello World!");
+//            Order order1 = new Order(1, 100);
+//            User user = new User("email@email.com", "secret");
+        
+        /*Race race = new Race();
+        race.Begin();*/
+        
+        /*Exceptions exceptions = new Exceptions();*/
+        
+//        var lambdaExpressions = new LambdaExpressions();
+//        lambdaExpressions.Test();
+
+            var eventSandBox = new EventSandBox();
+            eventSandBox.Test();
+        }
+    }
+}
