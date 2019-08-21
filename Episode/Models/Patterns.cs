92fb8e9892169f9c6c7dd4c4368e1d88dd61039d
martut <martut2762@gmail.com>
2019-08-21 15:08:22 +0200

    Patterns

--- Episode/Models/Patterns.cs
+++ Episode/Models/Patterns.cs
@@ -2,6 +2,40 @@ namespace Episode1.Models
 {
     public class Patterns
     {
+        //SOLID - 
+            /*    S -> SINGLE RESPONSIBILITY PRINCIPLE - nasze klasy/moetody powinny zajmować się jedną funkcjonalnością
+             *    O -> OPEN/CLOSED PRINCIPLE -
+             *    L -> LISKOVS SUBSTITION PRINCIPLE
+             *    I -> INTERFACE SEGREGATION PRINCIPLE
+             *    D -> DEPENDENCY INVERSION PRINCIPLE -> DEPENDENCY INJECTION
+             */
+        //YAGNI - YOU ARENT GONNA NEED IT - Nie pisać tego na wyrost czego aktualnie nie bedziesz używał
+        //DRY - DONT REPEAT YOURSELF - Jeżeli powtarzamy się starajmy się tworzyć spólny interfejs,klase,metode.
+        //KISS - KEEP IT SIMPLE STUPID - któtkie metody dobrze nazwane, pisać tak jak książkę od góry do dołu, starać się pisać prosty kod tak aby inni rozumieli.
+        //GRASP !!!
         
+        
+     /*`
+      Patterns
+      Inversion of Control -> Hollywood Principle - dont call as we will call you
+      Strategy Pattern
+      Repository Pattern
+      Proxy, Wrapper, Facade Pattern
+      Singleton - nie robi sie samemu deleguje sie do kontenera IOC poniewaz singleton jest trundnym wzorcem wchodza w jego sklad
+      takie zagadniuenia jak wielowatkowość, równoległość. trzeba go bardzo dobrze przemyslec
+      
+      Adapter pattern
+      
+      Command handler / event handler
+      
+      CQS -> Command and Query Separation -> CQRS
+      
+      
+      
+      
+      
+      
+      
+      */   
     }
 }
